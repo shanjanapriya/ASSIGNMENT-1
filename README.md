a13011d1a2282ed9849adee88f13d79a899ef2e4
shanjanapriya <priyashanjana2305@gmail.com>
2024-11-17 15:08:10 +0600

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+IT's MY Assignment.
+
+## Tecnologies
+
+Live link : https://shanjanapriya.github.io/ASSIGNMENT-1/
---
+++ js/ex1.js
@@ -0,0 +1,29 @@
+document.getElementById("student_message").innerHTML="Hi,my name is priya, I'm 20 years old and I'm learning Javascript";
+
+var num_1=101;
+var num_2=7
+var division = num_1 /num_2;
+console.log(division.toFixed(2));
+
+
+document.getElementById("result").innerHTML=division.toFixed(2);
+
+var phone1 = "988866552";
+if( phone1.length ==9 ) {
+    console.log(phone1+" valid");
+
+}else{
+    console.log(phone1 + "invalid");
+}
+
+
+
+var phone2 = "99087612366";
+if( phone1.length ==9 ) {
+    console.log(phone1+" valid");
+
+}else{
+    console.log(phone1 + "invalid");
+}
+
+
